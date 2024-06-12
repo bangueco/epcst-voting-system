5c61b5993cce449479030f2b9f9cfacea847a2a8
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-12 20:20:24 +0800

    feat: add home page

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native"
+
+const Home = () => {
+  return (
+    <View>
+      <Text>
+        Kid
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+})
+
+export default Home
