963c544fd6efeeed5a4157304da3f2ee6f4c5777
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-12 20:20:00 +0800

    refactor: layout for page navigations

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,20 @@
-import { Stack } from "expo-router";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import TabLayout from './(tabs)/_layout';
+import Login from './login'
+import Register from './register'
+
+const Stack = createNativeStackNavigator()
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{
-      contentStyle: {backgroundColor: '#31363F'}
-    }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login" options={{headerShown: false}} />
-    </Stack>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator screenOptions={{contentStyle: {backgroundColor: '#31363F'}}}>
+        <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+        <Stack.Screen name="Main" component={TabLayout} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
