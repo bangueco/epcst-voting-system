7f942d25d34ac975bc6fc977df344f78fd1ec3d6
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-12 20:20:38 +0800

    feat: add profile page

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>
+        Kid
+      </Text>
+    </View>
+  )
+}
+
+export default Profile
