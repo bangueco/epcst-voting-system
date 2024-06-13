fa2336b6b0f90161cc6fc13ead6de24c9ac99cb8
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-13 23:20:25 +0800

    feat: redirect users when authenticated on main page

--- app/register.tsx
+++ app/register.tsx
@@ -1,13 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { registerUser } from "../services/authentication"
 import { Button, StyleSheet, Text, TextInput, View, Alert, Pressable } from "react-native"
 import { useNavigation, NavigationProp } from "@react-navigation/native";
 
+import { app } from "../firebaseConfig"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 const Register = () => {
+  const auth = getAuth(app)
   const navigation = useNavigation<NavigationProp<any>>();
 
+  const [authUser, setAuthUser] = useState()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user:any) => {
+      if (user) {
+        setAuthUser(user)
+      } else {
+        setAuthUser(undefined)
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    if (authUser) {
+      navigation.navigate("Main")
+    }
+  }, [authUser])
 
   const onPressRegister = async () => {
     try {
@@ -23,44 +44,50 @@ const Register = () => {
     }
   }
 
+  if (!authUser) {
 
-  return (
-    <>
-
-      {/* TODO: Change user interface when backend is fully functional */}
-
-      <View style={styles.container}>
-        <View style={styles.registerContainer}>
-          <Text style={{textAlign: 'center'}}>EPCST VOTING SYSTEM</Text>
-          <View>
+    return (
+      <>
+  
+        {/* TODO: Change user interface when backend is fully functional */}
+  
+        <View style={styles.container}>
+          <View style={styles.registerContainer}>
+            <Text style={{textAlign: 'center'}}>EPCST VOTING SYSTEM</Text>
             <View>
-              <Text>Username</Text>
-              <TextInput
-                style={styles.textInput}
-                onChangeText={(e) => setEmail(e)}
-              />
+              <View>
+                <Text>Username</Text>
+                <TextInput
+                  style={styles.textInput}
+                  onChangeText={(e) => setEmail(e)}
+                />
+              </View>
+              <View>
+                <Text>Password</Text>
+                <TextInput
+                  style={styles.textInput}
+                  secureTextEntry={true}
+                  onChangeText={(e) => setPassword(e)}
+                />
+              </View>
             </View>
-            <View>
-              <Text>Password</Text>
-              <TextInput
-                style={styles.textInput}
-                secureTextEntry={true}
-                onChangeText={(e) => setPassword(e)}
-              />
-            </View>
+            <Button 
+              title="Register"
+              onPress={onPressRegister}
+            />
+            <Pressable onPress={() => navigation.navigate("Login")}>
+              <Text>Already have account? Login here</Text>
+            </Pressable>
           </View>
-          <Button 
-            title="Register"
-            onPress={onPressRegister}
-          />
-          <Pressable onPress={() => navigation.navigate("Login")}>
-            <Text>Already have account? Login here</Text>
-          </Pressable>
         </View>
-      </View>
+  
+      </>
+    )
 
-    </>
-  )
+  } else {
+    return null
+  }
+
 }
 
 const styles = StyleSheet.create({
