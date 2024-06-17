e9633023a387a51b075d52329909b1efadb9487f
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-17 21:34:14 +0800

    f

--- services/database.ts
+++ services/database.ts
@@ -3,10 +3,11 @@ import { getDatabase, ref, set } from "firebase/database"
 
 const db = getDatabase(app)
 
-const writeUserData = (uid:any, email:any, password:any) => {
+const writeUserData = (uid:any, email:any, username:any, password:any) => {
   set(ref(db, `users/${uid}`), {
     email: email,
-    password: password
+    password: password,
+    user_type: 'normal'
   })
 }
 
