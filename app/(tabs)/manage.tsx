3d2b02c17f66325d241db40ca86de1107e742d02
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-20 16:16:32 +0800

    feat: add functionality to add candidates to database and display them

--- app/(tabs)/manage.tsx
+++ app/(tabs)/manage.tsx
@@ -1,13 +1,45 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Alert, Button, Pressable, StyleSheet, Text, TextInput, View } from "react-native"
 import { writeStudentData } from "@/services/database"
+import { DataTable } from 'react-native-paper'
+import { FontAwesome } from '@expo/vector-icons'
+import {app} from "../../firebaseConfig"
+import { getDatabase, onValue, ref } from "firebase/database"
+
+interface Student {
+  id: string;
+  grade: string;
+  name: string;
+  party: string;
+  position: string;
+}
 
 const Manage = () => {
 
+  const db = getDatabase(app)
+
+  const [students, setStudents] = useState<Student[]>([])
   const [studentName, setStudentName] = useState('')
   const [studentGrade, setStudentGrade] = useState('')
   const [studentPosition, setStudentPosition] = useState('')
   const [studentParty, setStudentParty] = useState('')
+
+  useEffect(() => {
+
+    const fetchStudents = async () => {
+      const students = await ref(db, '/students')
+      onValue(students, (snapshot) => {
+        const data = snapshot.val()
+        const studentsArray = Object.keys(data).map(key => ({
+          id: key,
+          ...data[key],
+        }));
+        setStudents(studentsArray)
+      });
+    }
+
+    fetchStudents()
+  }, [])
 
   const onPressWriteStudent = () => {
     writeStudentData(studentName, studentGrade, studentPosition, studentParty)
@@ -59,6 +91,27 @@ const Manage = () => {
           <Text style={{fontSize: 20, color: 'white'}}>Submit</Text>
         </Pressable>
       </View>
+      <View>
+        <DataTable style={styles.tableContainer}>
+          <DataTable.Header style={styles.tableHeader}> 
+            <DataTable.Title>Name</DataTable.Title> 
+            <DataTable.Title>Grade</DataTable.Title> 
+            <DataTable.Title>Position</DataTable.Title>
+            <DataTable.Title>Actions</DataTable.Title>
+          </DataTable.Header>
+          {students.map(student => (
+            <DataTable.Row key={student.id}>
+              <DataTable.Cell>{student.name}</DataTable.Cell>
+              <DataTable.Cell>{student.grade}</DataTable.Cell> 
+              <DataTable.Cell>{student.position}</DataTable.Cell>
+              <DataTable.Cell>
+                <FontAwesome name="edit" size={24} color="blue" />
+                <FontAwesome name="trash-o" size={24} color="red" />
+              </DataTable.Cell>
+            </DataTable.Row>
+          ))}
+        </DataTable>
+      </View>
     </View>
   )
 }
@@ -79,6 +132,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'green',
     width: '20%'
+  },
+  tableContainer: { 
+    padding: 10,
+    textAlign: 'center'
+  }, 
+  tableHeader: { 
+    backgroundColor: '#DCDCDC', 
+  },
+  cellActions: {
+    display: 'flex',
+    gap: 50
   }
 })
 
